b93289872196dafc6477907ecf4ff2488525d358
Igor C. Menin <igormenin@gmail.com>
2023-07-10 21:55:50 -0300

    ORM and Seed BD
    Correções do ORM, e população da tabela Games.

--- src/main/java/com/igormenin/dslist/entities/Game.java
+++ src/main/java/com/igormenin/dslist/entities/Game.java
@@ -20,25 +20,31 @@ public class Game {
 	@Column(name = "gameYear")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
+	
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
+	private Double score;
 	
 	public Game() {
 		
 	}
 
-	public Game(long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescription, String longDescription) {
+	public Game(long id, String title, Integer year, String genre, String platforms, String imgUrl,
+			String shortDescription, String longDescription, Double score) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
 		this.longDescription = longDescription;
+		this.score = score;
 	}
 
 	public long getId() {
@@ -74,11 +80,11 @@ public class Game {
 	}
 
 	public String getPlatform() {
-		return platform;
+		return platforms;
 	}
 
 	public void setPlatform(String platform) {
-		this.platform = platform;
+		this.platforms = platform;
 	}
 
 	public String getImgUrl() {
@@ -91,6 +97,16 @@ public class Game {
 
 	public String getShortDescription() {
 		return shortDescription;
+	}
+	
+	
+	
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public void setShortDescription(String shortDescription) {
